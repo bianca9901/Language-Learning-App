ad35ab50d1d8c544bc813114ad2f05610bd30b81
bianca9901 <biancawikberg99@yahoo.com>
2024-03-18 12:14:12 +0100

    Create initial draft of Sidebar component

---
+++ components/ui/sidebar.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+type Props = {
+  className?: string;
+};
+
+const Sidebar = ({ className }: Props) => {
+  return (
+    <div
+      className={cn(
+        `bg-blue-500 h-full lg:w-[256px] lg:fixed flex
+        left-0 top-0 px-4 border-r-2 flex-col`,
+        className
+      )}
+    ></div>
+  );
+};
+
+export default Sidebar;
