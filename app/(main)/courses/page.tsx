a0cf7b7d28a4c8c7a8d37ed20b20e2d6867a86e0
bianca9901 <biancawikberg99@yahoo.com>
2024-03-19 10:03:09 +0100

    Refactor Courses page to fetch data concurrently using Promise.all

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -3,14 +3,22 @@ import React from 'react'
 import List from './list'
 
 const CoursesPage = async () => {
-    const data = await getCourses()
-    const userProgress = await getUserProgress()
+    const coursesData = getCourses()
+    const userProgressData = getUserProgress()
+
+    const [
+      courses,
+      userProgress
+    ] = await Promise.all([
+      coursesData,
+      userProgressData,
+    ])
 
   return (
     <div className='h-full max-w-[912px] px-3 mx-auto'>
         <h1 className='text-xl font-bold text-neutral-700'>Language Courses
         </h1>
-        <List courses={data} activeCourseId={userProgress?.activeCourseId} />
+        <List courses={courses} activeCourseId={userProgress?.activeCourseId} />
     </div>
   )
 }
