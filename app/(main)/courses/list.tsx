86e43bd2b5eab4dc28bfd0152028c2aee0cc79a5
bianca9901 <biancawikberg99@yahoo.com>
2024-03-19 11:12:08 +0100

    Add onClick logic to List component
    The onClick function checks if there is a pending transition and redirects to the '/learn' page if the clicked course is already active. Otherwise, it initiates the upsertUserProgress function to handle the creation or update of user progress for the clicked course.

--- app/(main)/courses/list.tsx
+++ app/(main)/courses/list.tsx
@@ -1,7 +1,9 @@
 "use client"
 import { courses, userProgress } from '@/db/schema'
-import React from 'react'
+import React, { useTransition } from 'react'
 import Card from './card';
+import { useRouter } from 'next/navigation';
+import { upsertUserProgress } from '@/actions/user-progress';
 
 type Props = {
     courses: typeof courses.$inferSelect[];
@@ -9,12 +11,26 @@ type Props = {
 }
 
 function List({courses, activeCourseId}: Props) {
+  const router = useRouter()
+  const [pending, startTransition] = useTransition()
+
+  const onClick = (id: number) => {
+    if(pending) return
+    if(id === activeCourseId) {
+      return router.push("/learn")
+    }
+
+    startTransition(() => {
+      upsertUserProgress(id)
+    })
+  }
+
   return (
     <div className="pt-6 grid grid-cols-2 
     lg:grid-cols-[repeat(auto-fill,minmax(210px,1fr))] gap-4">
         {courses.map((course) => (
             <Card key={course.id} id={course.id} title={course.title}
-            imageSrc={course.imageSrc} onClick={() => {}} disabled={false} 
+            imageSrc={course.imageSrc} onClick={onClick} disabled={false} 
             active={course.id === activeCourseId}/>
         ))}
     </div>
