9d4f8924afb61befa5271f2d2d9040884ee59ccf
bianca9901 <biancawikberg99@yahoo.com>
2024-03-19 10:14:31 +0100

    Add logic to Learn page for user progress validation
    It fetches user progress data and ensures that the activeCourse is available. If user progress data or activeCourse is not available, it redirects the user to the '/courses' page. This logic enhances user experience by ensuring users only can access the Learn page when they have made progress in a course.

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,9 +2,24 @@ import FeedWrapper from "@/components/ui/feed-wrapper";
 import StickyWrapper from "@/components/ui/sticky-wrapper";
 import React from "react";
 import Header from "./header";
+import { getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
 import UserProgress from "@/components/ui/user-progress";
 
-const LearnPage = () => {
+
+const LearnPage = async () => {
+  const userProgressData = getUserProgress()
+  
+  const [
+    userProgress
+  ] = await Promise.all([
+    userProgressData
+  ])
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect('/courses')
+  }
+
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
