1ed256eab930c9b0b0e5150c797c2db8ea22e511
bianca9901 <biancawikberg99@yahoo.com>
2024-03-19 10:39:35 +0100

    Add getCourseById function to fetch course data by its ID from the database

--- db/queries.ts
+++ db/queries.ts
@@ -2,7 +2,7 @@ import { cache } from "react";
 import db from "./drizzle";
 import { auth } from "@clerk/nextjs";
 import { eq } from "drizzle-orm";
-import { userProgress } from "./schema";
+import { courses, userProgress } from "./schema";
 
 export const getUserProgress = cache(async () => {
   const { userId } = await auth()
@@ -23,3 +23,11 @@ export const getCourses = cache(async () => {
   const data = await db.query.courses.findMany();
   return data;
 });
+
+export const getCourseById = cache(async (courseId: number) => {
+  const data = await db.query.courses.findFirst({
+    where: eq(courses.id, courseId),
+    // Todo: populate units ans lessons
+  })
+  return data;
+})
