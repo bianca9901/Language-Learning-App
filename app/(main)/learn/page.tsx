2d4f464a981c1a9b2f5195d3998a6ad3219855e6
bianca9901 <biancawikberg99@yahoo.com>
2024-03-19 12:54:56 +0100

    Replace hardcoded values with dynamic values

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -6,28 +6,28 @@ import { getUserProgress } from "@/db/queries";
 import { redirect } from "next/navigation";
 import UserProgress from "@/components/ui/user-progress";
 
-
 const LearnPage = async () => {
-  const userProgressData = getUserProgress()
-  
-  const [
-    userProgress
-  ] = await Promise.all([
-    userProgressData
-  ])
+  const userProgressData = getUserProgress();
+
+  const [userProgress] = await Promise.all([userProgressData]);
 
   if (!userProgress || !userProgress.activeCourse) {
-    redirect('/courses')
+    redirect("/courses");
   }
 
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
-        <UserProgress activeCourse={{title: "Spanish", imageSrc: "/es.svg"}} hearts={5} points={100} hasActiveSubscription={false} />
+        <UserProgress
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
+          hasActiveSubscription={false}
+        />
       </StickyWrapper>
-      
+
       <FeedWrapper>
-        <Header title="Spanish"/>
+        <Header title={userProgress.activeCourse.title} />
       </FeedWrapper>
     </div>
   );
